509719e811376b4b85859a15c9a451687b0d2b97
Josh.5 <jsunnex@gmail.com>
2019-09-29 13:19:47 +1300

    [WEBUI] Move main dashboard template pages to subfolders

--- webserver/main.py
+++ webserver/main.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-###################################################################################################
-#
-#   Written by:               Josh.5 <jsunnex@gmail.com>
-#   Date:                     Thu Dec 06 2018, (7:21:18 AM)
-#
-#   Copyright:
-#          Copyright (C) Josh Sunnex - All Rights Reserved
-#
-#          Permission is hereby granted, free of charge, to any person obtaining a copy
-#          of this software and associated documentation files (the "Software"), to deal
-#          in the Software without restriction, including without limitation the rights
-#          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#          copies of the Software, and to permit persons to whom the Software is
-#          furnished to do so, subject to the following conditions:
-# 
-#          The above copyright notice and this permission notice shall be included in all
-#          copies or substantial portions of the Software.
-# 
-#          THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#          EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-#          MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-#          IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-#          DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-#          OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-#          OR OTHER DEALINGS IN THE SOFTWARE.
-#
-#
-###################################################################################################
 
+"""
+    unmanic.main.py
+
+    Written by:               Josh.5 <jsunnex@gmail.com>
+    Date:                     06 Dec 2018, (7:21 AM)
+
+    Copyright:
+           Copyright (C) Josh Sunnex - All Rights Reserved
+
+           Permission is hereby granted, free of charge, to any person obtaining a copy
+           of this software and associated documentation files (the "Software"), to deal
+           in the Software without restriction, including without limitation the rights
+           to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+           copies of the Software, and to permit persons to whom the Software is
+           furnished to do so, subject to the following conditions:
+
+           The above copyright notice and this permission notice shall be included in all
+           copies or substantial portions of the Software.
+
+           THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+           EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+           MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+           IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+           DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+           OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+           OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
 
 import time
 import tornado.web
@@ -38,19 +38,25 @@ from lib import history
 
 
 class MainUIRequestHandler(tornado.web.RequestHandler):
+    name = None
+    data_queues = None
+    workerHandle = None
+    components = None
+    config = None
+
     def initialize(self, data_queues, workerHandle, settings):
-        self.name           = 'main'
-        self.data_queues    = data_queues
-        self.workerHandle   = workerHandle
-        self.components     = []
-        self.config         = settings
+        self.name = 'main'
+        self.data_queues = data_queues
+        self.workerHandle = workerHandle
+        self.components = []
+        self.config = settings
 
     def get(self, path):
         if self.get_query_arguments('ajax'):
             # Print out the json based on the call
             self.handle_ajax_call(self.get_query_arguments('ajax')[0])
         else:
-            self.render("main.html", time_now=time.time())
+            self.render("main/main.html", time_now=time.time())
 
     def handle_ajax_call(self, query):
         self.set_header("Content-Type", "application/json")
@@ -59,13 +65,13 @@ class MainUIRequestHandler(tornado.web.RequestHandler):
         if query == 'pendingTasks':
             if self.get_query_arguments('format') and 'html' in self.get_query_arguments('format'):
                 self.set_header("Content-Type", "text/html")
-                self.render("main-pending-tasks.html", time_now=time.time())
+                self.render("main/main-pending-tasks.html", time_now=time.time())
             else:
                 self.write(json.dumps(self.get_pending_tasks()))
         if query == 'historicalTasks':
             if self.get_query_arguments('format') and 'html' in self.get_query_arguments('format'):
                 self.set_header("Content-Type", "text/html")
-                self.render("main-completed-tasks-list.html", time_now=time.time())
+                self.render("main/main-completed-tasks-list.html", time_now=time.time())
             else:
                 self.write(json.dumps(self.get_historical_tasks()))
 
@@ -81,4 +87,3 @@ class MainUIRequestHandler(tornado.web.RequestHandler):
     def get_historical_tasks(self):
         history_logging = history.History(self.config)
         return history_logging.read_history_log()
-

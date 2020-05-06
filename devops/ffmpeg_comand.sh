992c3294013833906f2f671112638d706be0582a
Josh.5 <jsunnex@gmail.com>
2020-05-06 13:14:14 +1200

    [POSTPROCESSOR] Dont raise exception when file fails postprocess validation and it is not the correct codec. TODO: This needs to be modified so that the codec is not read from the settings, but rather the task. Modifying the settings mid conversion would cause this validation check to fail

--- devops/ffmpeg_comand.sh
+++ devops/ffmpeg_comand.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
-IN_FILE=$(realpath ${SCRIPT_PATH}/../tests/support_/videos/med/big_buck_bunny_720p_10mb.mp4);
 OUT_DIR=$(realpath ${SCRIPT_PATH}/../tests/tmp/ffmpeg_tests);
+IN_FILE=$(realpath ${SCRIPT_PATH}/../tests/support_/videos/med/big_buck_bunny_720p_10mb.mp4);
 
-
+# Set the container
+CONTAINER_EXT="mkv"
 # Select video encoder:
 #   - libx265
 #   - nvenc_hevc (depreciated)
 #   - hevc_nvenc
-#   - hevc_vaapi
-V_ENCODER="nvenc_hevc"
+#   - h264_nvenc
+V_ENCODER="hevc_nvenc"
+# Additional encoder args
+#   EG:
+#       -b:v 5M
+#       -b:v 1M -maxrate 2M -bufsize 4M -preset slow
+V_ENCODER_ARGS=""
 
 
 
@@ -38,16 +44,29 @@ elif [[ ! -z ${1} && "${1}" == "encoders" ]]; then
     exit 0
 else
     mkdir -p ${OUT_DIR}
-    CMD="ffmpeg -i ${IN_FILE} \
+    CMD="ffmpeg \
+        -i ${IN_FILE} \
         -map 0:0 \
         -map 0:1 \
         -c:v ${V_ENCODER} \
-        -c:a:1 libmp3lame -b:a:0 192k -ac 2 \
-        -y ${OUT_DIR}/outfile.mkv"
+        ${V_ENCODER_ARGS} \
+        -c:a copy \
+        -y ${OUT_DIR}/outfile.${CONTAINER_EXT}"
 
+    start_time=`date +%s`
     echo "${CMD}"
     bash -c "${CMD}"
+    end_time=`date +%s`
 
-    ls -l ${OUT_DIR}/
+    echo
+    echo
+    echo "Task took $((end_time-start_time)) seconds"
+    echo
+    echo "Input file:"
+    du -h ${IN_FILE}
+    echo
+    echo "Output file:"
+    du -h ${OUT_DIR}/*.${CONTAINER_EXT}
+    echo
     exit 0
 fi

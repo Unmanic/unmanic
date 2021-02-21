46448894f7f779f83514dc15cd5e0f4bc0a869b4
Josh.5 <jsunnex@gmail.com>
2021-02-21 15:36:07 +1300

    Fix formatting errors in various python files

--- unmanic/libs/singleton.py
+++ unmanic/libs/singleton.py
@@ -30,12 +30,14 @@
 
 """
 
-
 """
 Singleton metaclass
 """
+
+
 class SingletonType(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(SingletonType, cls).__call__(*args, **kwargs)

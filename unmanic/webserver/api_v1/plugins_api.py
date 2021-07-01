926b1423aa0eb43876ad8c80a8c01f18a23d9acd
Josh.5 <jsunnex@gmail.com>
2021-07-02 10:09:13 +1200

    [PLUGINS] Add support for long description in a 'description.txt' file located in the plugin directory. This will concatinate to the description entry in the info.json which is limited to adding everyting in a single line due to it's JSON syntax

--- unmanic/webserver/api_v1/plugins_api.py
+++ unmanic/webserver/api_v1/plugins_api.py
@@ -387,6 +387,17 @@ class ApiPluginsHandler(BaseApiHandler):
         plugin_executor = PluginExecutor()
         return plugin_executor.get_plugin_changelog(plugin_id)
 
+    def __get_plugin_long_description(self, plugin_id):
+        """
+        Given a plugin ID , return a list of lines read from the plugin's changelog
+
+        :param plugin_id:
+        :return:
+        """
+        # Fetch plugin changelog
+        plugin_executor = PluginExecutor()
+        return plugin_executor.get_plugin_long_description(plugin_id)
+
     def __get_plugin_info_and_settings(self, plugin_id):
         plugins = PluginsHandler()
 
@@ -425,6 +436,7 @@ class ApiPluginsHandler(BaseApiHandler):
             if plugin_installed:
                 plugin_data['settings'] = self.__get_plugin_settings(plugin_result.get('plugin_id'))
                 plugin_data['changelog'] = "".join(self.__get_plugin_changelog(plugin_result.get('plugin_id')))
+                plugin_data['description'] += "\n" + "".join(self.__get_plugin_long_description(plugin_result.get('plugin_id')))
             break
 
         return plugin_data

9a0d93d8282ddda26fb6216d12ced581b04f2e45
Josh.5 <jsunnex@gmail.com>
2021-07-04 10:49:34 +1200

    [WEBUI] Initial push to migrate frontend to NPM dependencies

--- unmanic/webserver/assets/global/scripts/helpers.js
+++
@@ -1,8 +0,0 @@
-
-const toggleCheckboxHiddenInput = function (checkbox_id, hidden_input_id) {
-    if ($("#" + checkbox_id).prop("checked")) {
-        $("#" + hidden_input_id).val("true");
-    } else {
-        $("#" + hidden_input_id).val("false");
-    }
-};

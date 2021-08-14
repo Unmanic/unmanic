fb5e0d960c7a7279a6cdae76b3d843bd71559e69
Josh.5 <jsunnex@gmail.com>
2021-08-14 18:32:08 +1200

    [API] Add filebrowser API v2 endpoint. This allows frontend file and directory browsing.

--- unmanic/webserver/api_v2/filebrowser_api.py
+++ unmanic/webserver/api_v2/filebrowser_api.py
@@ -29,3 +29,98 @@
            OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
+import os
+
+import tornado.log
+from unmanic.libs import session
+from unmanic.libs.uiserver import UnmanicDataQueues
+from unmanic.webserver.api_v2.base_api_handler import BaseApiHandler, BaseApiError
+from unmanic.webserver.api_v2.schema.schemas import DirectoryListingResultsSchema, DocumentContentSuccessSchema, \
+    RequestDirectoryListingDataSchema
+from unmanic.webserver.helpers.filebrowser import DirectoryListing
+
+
+class ApiFilebrowserHandler(BaseApiHandler):
+    session = None
+    params = None
+    unmanic_data_queues = None
+
+    routes = [
+        {
+            "path_pattern":      r"/filebrowser/list",
+            "supported_methods": ["POST"],
+            "call_method":       "fetch_directory_listing",
+        }
+    ]
+
+    def initialize(self, **kwargs):
+        self.session = session.Session()
+        self.params = kwargs.get("params")
+        udq = UnmanicDataQueues()
+        self.unmanic_data_queues = udq.get_unmanic_data_queues()
+
+    def fetch_directory_listing(self):
+        """
+        Filebrowser - List files and/or subdirectories in a given directory
+        ---
+        description: Returns a list of files and/or subdirectories in a given directory.
+        requestBody:
+            description: Requested a list of files and/or subdirectories in a given directory.
+            required: True
+            content:
+                application/json:
+                    schema:
+                        RequestDirectoryListingDataSchema
+        responses:
+            200:
+                description: 'Sample response: Returns a list of files and/or subdirectories in a given directory.'
+                content:
+                    application/json:
+                        schema:
+                            DirectoryListingResultsSchema
+            400:
+                description: Bad request; Check `messages` for any validation errors
+                content:
+                    application/json:
+                        schema:
+                            BadRequestSchema
+            404:
+                description: Bad request; Requested endpoint not found
+                content:
+                    application/json:
+                        schema:
+                            BadEndpointSchema
+            405:
+                description: Bad request; Requested method is not allowed
+                content:
+                    application/json:
+                        schema:
+                            BadMethodSchema
+            500:
+                description: Internal error; Check `error` for exception
+                content:
+                    application/json:
+                        schema:
+                            InternalErrorSchema
+        """
+        try:
+            json_request = self.read_json_request(RequestDirectoryListingDataSchema())
+
+            directory_listing = DirectoryListing(json_request.get('list_type', 'all'))
+            path_data = directory_listing.fetch_path_data(json_request.get('current_path', '/'))
+
+            response = self.build_response(
+                DirectoryListingResultsSchema(),
+                {
+                    'directories': path_data.get('directories', []),
+                    'files':       path_data.get('files', []),
+                }
+            )
+            self.write_success(response)
+            return
+        except BaseApiError as bae:
+            tornado.log.app_log.error("BaseApiError.{}: {}".format(self.route.get('call_method'), str(bae)))
+            return
+        except Exception as e:
+            self.set_status(self.STATUS_ERROR_INTERNAL, reason=str(e))
+            self.write_error()

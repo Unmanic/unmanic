d23cecd44eddf33f8b31b7f86849cdde8f88273f
Josh.5 <jsunnex@gmail.com>
2024-08-31 12:32:14 +1200

    Update tornado to 6.2

--- setup.py
+++ setup.py
@@ -58,7 +58,6 @@ module_classifiers = [
     versioninfo.dev_status(),
     'Intended Audience :: End Users/Desktop',
     'Intended Audience :: Developers',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',

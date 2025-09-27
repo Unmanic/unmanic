3d9dd745e53da0eba0893b4d4ed5f668510723db
Josh.5 <jsunnex@gmail.com>
2025-09-27 17:48:22 +1200

    Fix exception in integration tests

--- unmanic/libs/taskhandler.py
+++ unmanic/libs/taskhandler.py
@@ -34,6 +34,8 @@ import queue
 import threading
 import time
 
+from peewee import OperationalError
+
 from unmanic import config
 from unmanic.libs import common, task
 from unmanic.libs.logs import UnmanicLogging
@@ -127,19 +129,22 @@ class TaskHandler(threading.Thread):
         if not self.settings.get_clear_pending_tasks_on_restart():
             # Exclude all pending tasks except for those that are remote tasks... They need to be removed
             where_clause = (Tasks.status != 'pending') | (Tasks.type == 'remote')
-        # Get all task IDs to be deleted
-        select_query = Tasks.select(Tasks.id)
-        if where_clause is not None:
-            select_query = select_query.where(where_clause)
-        # Remove any task data associated with the tasks
-        for (task_id,) in select_query.tuples().iterator():
-            task.TaskDataStore.clear_task(task_id)
-        # Delete the tasks
-        delete_query = Tasks.delete()
-        if where_clause is not None:
-            delete_query = delete_query.where(where_clause)
-        rows_deleted_count = delete_query.execute()
-        self._log("Deleted {} items from tasks list".format(rows_deleted_count), level='debug')
+        try:
+            # Get all task IDs to be deleted
+            select_query = Tasks.select(Tasks.id)
+            if where_clause is not None:
+                select_query = select_query.where(where_clause)
+            # Remove any task data associated with the tasks
+            for (task_id,) in select_query.tuples():
+                task.TaskDataStore.clear_task(task_id)
+            # Delete the tasks
+            delete_query = Tasks.delete()
+            if where_clause is not None:
+                delete_query = delete_query.where(where_clause)
+            rows_deleted_count = delete_query.execute()
+            self._log("Deleted {} items from tasks list".format(rows_deleted_count), level='debug')
+        except OperationalError as error:
+            self._log("Skipping task cleanup at startup; tasks table missing", str(error), level='debug')
 
     @staticmethod
     def check_if_task_exists_matching_path(abspath):

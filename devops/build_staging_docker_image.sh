b89ed14875e713a5a62d9906b338c97d01a987b8
Josh.5 <jsunnex@gmail.com>
2026-06-15 14:27:53 +1200

    Pull in changes from frontend project

---
+++ devops/build_staging_docker_image.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+###################################################################################################
+#
+#   Written by:               Josh.5 <jsunnex@gmail.com>
+#   Date:                     Thu Jan 07 2019, (17:59:00 PM)
+#
+#   Copyright:
+#          Copyright (C) Josh Sunnex - All Rights Reserved
+#
+#          Permission is hereby granted, free of charge, to any person obtaining a copy
+#          of this software and associated documentation files (the "Software"), to deal
+#          in the Software without restriction, including without limitation the rights
+#          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#          copies of the Software, and to permit persons to whom the Software is
+#          furnished to do so, subject to the following conditions:
+#
+#          The above copyright notice and this permission notice shall be included in all
+#          copies or substantial portions of the Software.
+#
+#          THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#          EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#          MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#          IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#          DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#          OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+#          OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+###################################################################################################
+
+set -eo pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+PROJECT_BASE=$(cd "$SCRIPT_DIR/.." && pwd)
+
+rm -rfv ${PROJECT_BASE:?}/build && rm -fv ${PROJECT_BASE:?}/dist/unmanic-*
+
+python3 -m build --no-isolation --skip-dependency-check --wheel
+python3 -m build --no-isolation --skip-dependency-check --sdist
+
+docker build -f ${PROJECT_BASE:?}/docker/Dockerfile -t josh5/unmanic:staging -t ghcr.io/unmanic/unmanic:staging ${PROJECT_BASE:?}/
